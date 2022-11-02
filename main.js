949c74d5b47586b22de1161d89295d1e8202205d
Jessel Diaz <jesseljosuediazperez@hotmail.com>
2022-11-02 14:57:06 -0600

    new features have been added

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+const dateDay = document.querySelector('.todo__date-day')
+const dateMonth = document.querySelector('.todo__date-month')
+const dateWeekday = document.querySelector('.todo__date-weekday')
 const todoList = document.querySelector('.todo__list');
 const todoInput = document.querySelector('.todo__input');
-const addButton = document.querySelector('.fa-plus');
+const addButton = document.querySelector('.fa-plus')
+const panicButton = document.querySelector('#panic-button')
+const organizeButton = document.querySelector('#organize-button')
 
+
+panicButton.addEventListener('click', cleanList)
+organizeButton.addEventListener('click', renderOrganizeList)
 addButton.addEventListener('click', addItemList)
 
 function addItemList() {
@@ -17,7 +25,7 @@ function addItemList() {
     todoItem.appendChild(addCompleteButton())
     todoItem.appendChild(todoText);
     todoItem.appendChild(addDeleteButton())
-    todoList.appendChild(todoItem);
+    todoList.prepend(todoItem);
   }
   todoInput.value = ''
 }
@@ -43,8 +51,43 @@ function addCompleteButton () {
   
   completeTaskButton.addEventListener('click', (e) => {
     const item = e.target.nextElementSibling;
+    const listItem = e.target.parentElement;
+
+    listItem.classList.toggle('taskDone')
     item.classList.toggle('task-completed')
+
     completeTaskButton.classList.toggle('border-radius-on')
   })
   return completeTaskButton;
 }
+
+function cleanList () {
+  todoList.textContent = ''
+}
+
+function organizeList() {
+  const taskDone = []
+  const taskToDo = []
+
+  todoList.childNodes.forEach(listItem => {
+    if(listItem.classList.contains('taskDone')) {
+      taskDone.push(listItem)
+    } else {
+      taskToDo.push(listItem)
+    }
+  })
+  return [...taskToDo, ...taskDone]
+}
+
+function renderOrganizeList() {
+  organizeList().forEach(listItem => todoList.appendChild(listItem))
+}
+
+const setDate = () => {
+  const date = new Date()
+  dateDay.textContent = date.toLocaleString('en', { day: 'numeric' });
+  dateMonth.textContent = date.toLocaleString('en', { month: 'short' });
+  dateWeekday.textContent = date.toLocaleString('en', { weekday: 'long' });
+}
+
+setDate();
